5c24bc65546cfb18ee48d8a813886531184c15b3
ranjandhakal <ranjan.dhakal@infodev.com.np>
2024-08-01 12:05:12 +0545

    first commit

---
+++ cypress/e2e/classTwo.cy.js
@@ -0,0 +1,40 @@
+describe('Login Page Testing', () => {
+    it('Should be submitted ', () => {
+        cy.visit("https://ultimateqa.com/complicated-page") 
+        cy.get("#et_pb_contact_name_23").should('not.exist')
+        cy.get("#et_pb_contact_name_2").type("ranjan") //this is for name
+        cy.get('[placeholder="Email Address"]').eq(2).type("ranjan@gmail.com") //this is for email
+       cy.get("#et_pb_contact_message_2").type("This is class one message")
+       cy.get('[name="et_pb_contact_captcha_2"]').type("8")
+       cy.get("[type='submit']").eq(4).click()
+    });
+    it.only('class should not be exist', () => {
+        cy.viewport("iphone-x")
+        cy.visit("https://ultimateqa.com/complicated-page") 
+        cy.get("#et_pb_contact_name_2").should('exist')
+        cy.get("#et_pb_contact_name_2").should('have.attr','name','et_pb_contact_name_2')
+        cy.get("#et_pb_contact_name_2").should('have.class','input').should('be.visible')
+        cy.get('#et_pb_contact_name_2').should('be.empty')
+       
+        cy.url().should('equal',"https://ultimateqa.com/complicated-page")
+        cy.url().should('include','/complicated-page')
+
+    });
+    it('Second test',() => {
+        cy.visit("https://ultimateqa.com/complicated-page") 
+        cy.get("#et_pb_contact_name_2").should('exist')
+
+        cy.wait(10000)
+        cy.get("#et_pb_contact_name_2").should('have.attr','name','et_pb_contact_name_2')
+        cy.get("#et_pb_contact_name_2").should('have.class','input').should('be.visible')
+        cy.get('#et_pb_contact_name_2').should('be.empty')
+       
+        cy.url().should('equal',"https://ultimateqa.com/complicated-page")
+        cy.url().should('include','/complicated-page')
+    })
+
+});
+
+
+
+
